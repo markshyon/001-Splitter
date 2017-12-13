d57d674c1ead248231d269c730eb1dd7c8dbb48a
markshyon <hideen@gmail.com>
2017-12-13 22:19:38 +0800

    0.0.0.1

--- Splitter/ViewController.swift
+++ Splitter/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dinnerTF: UITextField!
+    @IBOutlet weak var totalTF: UITextField!
+    @IBOutlet weak var splitTF: UILabel!
+    @IBAction func calculate(_ sender: Any) {
+        let dinnerValue = Int(dinnerTF.text!)
+        let totalValue = Int(totalTF.text!)
+        @IBAction func dismissAC(_ sender: Any) {
+        }
+        
+        splitTF.text = String((Double(totalValue!) / Double(dinnerValue!)))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
