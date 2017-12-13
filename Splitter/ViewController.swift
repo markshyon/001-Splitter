6c25c7298642e790b5b2926a28523e8c77345a28
markshyon <hideen@gmail.com>
2017-12-13 20:04:40 +0800

    Initial Commit

---
+++ Splitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Splitter
+//
+//  Created by Shang Chi Cheng on 2017/12/13.
+//  Copyright © 2017年 shyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
